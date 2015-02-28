b680c48b73f22dbbab4bc456bbb88a589c5d01b5
Michael Kocherov <ppchaos@mail.ru>
2015-02-28 21:32:57 +0300

    Code simplified + ring buffer implemented

--- mlock_in_jack_apps/0/README.md
+++ mlock_in_jack_apps/0/README.md
@@ -1,1 +1,2 @@
-Compile with: g++ sample.cpp -ljack
+Program requires JACK devel package to be installed. On Ubuntu: sudo apt-get install libjack-jackd2-dev
+Compile with: g++ -Wall sample.cpp -ljack
--- mlock_in_jack_apps/0/sample.cpp
+++ mlock_in_jack_apps/0/sample.cpp
@@ -1,17 +1,12 @@
 #include <stdexcept>
 #include <string.h>
 #include <assert.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/select.h>
 #include <jack/jack.h>
 #include <iostream>
 
 class Client {
 public:
-	Client(): buffer_(0), payload_(0), jackClient_(0), jackPort_(0) {
-		pipeFds[0] = pipeFds[1] = -1;
-
+	Client(): buffer_(0), offset_(0), jackClient_(0), jackPort_(0) {
 		try {
 			init();
 		} catch(...) {
@@ -22,23 +17,13 @@ public:
 	~Client() {
 		release();
 	}
-	int getTermFd() const {
-		return pipeFds[0];
-	}
 private:
-	enum { BufferSize = 1 << 27 };
+	enum { BufferSize = 1014 * 1024 * 16 };
 	void init() {
 		buffer_ = new char[BufferSize];
 
-		if(pipe(pipeFds))
-			throw std::runtime_error("Failed to create communication pipe");
-
-		if(fcntl(pipeFds[1], F_SETFL, O_NONBLOCK)) // set non-blocking mode in order to avoid need of reading from pipeFds[0]
-			throw std::runtime_error("Failed to set non-blocking mode for write end of communication pipe");
-
 		jack_status_t status;
-		const std::string ClientName = "Test client";
-        jackClient_ = jack_client_open(ClientName.c_str(), static_cast<jack_options_t>(JackUseExactName), &status);
+		jackClient_ = jack_client_open("Test client", JackNullOption, &status);
 
 		if(!jackClient_)
 			throw std::runtime_error("Failed to create JACK client");
@@ -55,7 +40,7 @@ private:
 		if(jack_activate(jackClient_))
 			throw std::runtime_error("Failed to activate JACK client");
 
-		const std::string t = ClientName + ":" + PortName;
+		const std::string t = std::string(jack_get_client_name(jackClient_)) + ":" + PortName;
 
 		if(jack_connect(jackClient_, "system:capture_1", t.c_str()))
 			throw std::runtime_error("Failed to connect capture port to input port");
@@ -64,26 +49,27 @@ private:
  		if(jackClient_)
 			jack_client_close(jackClient_);
 
-		close(pipeFds[0]);
-		close(pipeFds[1]);
-
 		delete [] buffer_;
 	}
 	int processCallback_impl(const int theFramesCount) {
 		const int bytesCount = theFramesCount * sizeof(float); // JACK processes data in 32-bit float
-
-		if(payload_ + bytesCount > BufferSize) {
-			write(pipeFds[1], "q", 1);
-			return 0; // indicate stop for JACK
-		}
-
 		void * captureData = jack_port_get_buffer(jackPort_, theFramesCount);
 
 		if(captureData) {
+			int bytesToCopy = BufferSize - offset_;
+			bytesToCopy = bytesToCopy >= bytesCount ? bytesCount : bytesToCopy;
 			char * sink = buffer_;
-			sink += payload_;
-			memcpy(sink, reinterpret_cast<const char *>(captureData), bytesCount);
-			payload_ += bytesCount;
+			sink += offset_;
+			memcpy(sink, reinterpret_cast<const char *>(captureData), bytesToCopy);
+			offset_ += bytesToCopy;
+
+			if(bytesCount > bytesToCopy) {
+				const char * source = reinterpret_cast<const char *>(captureData);
+				source += bytesToCopy;
+				bytesToCopy = bytesCount - bytesToCopy;
+				memcpy(buffer_, source, bytesToCopy);
+				offset_ = bytesToCopy;
+			}
 		}
 
 		return 0;
@@ -95,21 +81,17 @@ private:
 	}
 private:
 	char * buffer_;
-	int payload_;
-	int pipeFds[2];
+	int offset_;
 	jack_client_t * jackClient_;
 	jack_port_t * jackPort_;
 };
 
 int main(int argc, char * argv[]) {
 	try {
-		Client client;
-		std::cout << "Press [Ctrl + D] or Enter to quit" << std::endl;
-		fd_set rfds;
-		FD_ZERO(&rfds);
-		FD_SET(0, &rfds); // wait for an event on stdin
-		FD_SET(client.getTermFd(), &rfds); // wait for termination event on client
-		select(client.getTermFd() + 1, &rfds, 0, 0, 0);
+		Client client[10];
+		(void)client;
+		std::cout << "Press Enter or Ctrl + D to quit" << std::endl;
+		std::cin.get();
 		return 0;
 	} catch(const std::exception & e) {
 		std::cerr << "Error: " << e.what() << std::endl;
